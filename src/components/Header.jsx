6f70370b995c105c9acec7dd9384c8770f692ce6
Nico <nmanograsso@gmail.com>
2020-04-14 23:02:15 -0300

    Add compact header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,16 @@ import React from "react"
 import "../assets/styles/components/Header.scss";
 import logo from '../assets/static/pokeball.png'
 
-const Header = () => (
-    <header className='header'>
-        <img className='header__image' src={logo} alt='header' />
-        <h1 className='header__title'>Pokédex</h1>
-    </header>
-)
+const Header = props => {
+    const { isCompact } = props
+    const headerClass = isCompact ? 'header isCompact' : 'header'
+
+    return(
+        <header className={headerClass}>
+            <img className='header__image' src={logo} alt='header' />
+            <h1 className='header__title'>Pokédex</h1>
+        </header>
+    )
+}
 
 export default Header
