28e7a19c80389fbbb0b6caf4d397a1128049afb5
Nico <nmanograsso@gmail.com>
2020-06-16 00:55:44 -0300

    Change anchor to Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import "../assets/styles/components/Header.scss";
 import logo from '../assets/static/pokeball.png'
 
@@ -9,7 +10,13 @@ const Header = props => {
     return(
         <header className={headerClass}>
             <img className='header__image' src={logo} alt='header' />
-            <h1 className='header__title'>Pokédex</h1>
+            {isCompact ?
+                <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+                    <h1 className='header__title' style={{cursor: 'pointer'}}>Pokédex</h1>
+                </Link>
+                :
+                <h1 className='header__title'>Pokédex</h1>
+            }
         </header>
     )
 }
