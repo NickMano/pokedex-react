16653fb4445d575e6303c6e3d8449b6c225d460a
Nico <nmanograsso@gmail.com>
2020-06-16 00:58:16 -0300

    Add actions and reducers

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {connect} from "react-redux"
+import {Link} from "react-router-dom";
+import {setLastPokemon} from "../actions"
+import useGetTypes from '../hooks/useGetTypes';
 import '../assets/styles/components/Card.scss'
 
 const Card = props => {
@@ -13,20 +17,23 @@ const Card = props => {
     }
 
     return(
-    <a className="card" href={link}>
+    <Link className="card" to={{pathname: `/pokemon/${name}`}} onClick={() => handleSetLastPokemon()}>
         <figure className="card__imageContainer">
             <img className="card__imageContainer--image" src={imageUrl} alt={name} />
         </figure>
         <div className="card__details">
-            <h3 className="card__title">{name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+            <h3 className="card__title">{name}</h3>
             <h4 className="card__number"># {number}</h4>
-            {(images.length > 0) &&<figure className="card__type-container">
+            {(images.length > 0) && <figure className="card__type-container">
                 {images.map( type => <img className="card__type-container--image" src={type.img} alt={type.name} />)}    
             </figure>}
         </div>
-    </a>
+    </Link>
 )}
 
+const mapDisptchToProps = {
+    setLastPokemon,
+}
 
 
-export default Card
+export default connect(null, mapDisptchToProps)(Card)
