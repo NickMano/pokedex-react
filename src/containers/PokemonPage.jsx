4bd7cf693c24befae6c1e561cf97633a65768e59
Nico <nmanograsso@gmail.com>
2020-06-17 23:22:55 -0300

    Parameters added dynamically

--- src/containers/PokemonPage.jsx
+++ src/containers/PokemonPage.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import useGetTypes from '../hooks/useGetTypes';
 import usePokemon from '../hooks/usePokemon';
 import '../assets/styles/components/PokemonDetails.scss'
+import { toCapitalized } from "../Utills";
 
 const PokemonPage = props => {
     const pokemon = usePokemon(props.match.params.id)
@@ -15,43 +16,40 @@ const PokemonPage = props => {
         <div style={{width: '100%', display: 'flex', justifyContent:'center', alignItems:'center'}} >
             <h1>#{number} - {props.match.params.id}&nbsp;</h1> 
             {(images.length > 0) &&
-                images.map( type => <img className="pokemon-detail__image-type" src={type.img} alt={type.name}/>)   
+                images.map( type => <img key={type.name} className="pokemon-detail__image-type" src={type.img} alt={type.name}/>)   
             }
         </div>
         <div className='grid-container'>
             <div className='foto'>
                 <img 
                     src={imageUrl} 
-                    alt='pokemon'
+                    alt={props.match.params.id}
                     style={{maxHeight:'96%', maxWidth:'96%', objectFit: 'contain'}}
                 />
             </div>
-            <div className='puntos'>
-                <h2>Puntos de Base</h2>
-                <h3 style={{margin:'10px 0'}}>PS: 106</h3>
-                <h3 style={{margin:'10px 0'}}>Ataque: 110</h3>
-                <h3 style={{margin:'10px 0'}}>Defensa: 90</h3>
-                <h3 style={{margin:'10px 0'}}>Ataque especial: 154</h3>
-                <h3 style={{margin:'10px 0'}}>Defensa especial: 90</h3>
-                <h3 style={{margin:'10px 0'}}>Velocidad: 130</h3>
+            <div className='stats'>
+                <h2>Stats</h2>
+                {stats 
+                ? stats.map( stat => <h3 key={stat.stat.name} style={{margin:'10px 0'}}>{toCapitalized(stat.stat.name)}: {stat.base_stat}</h3>)
+                : null}
             </div>
             <div className='datos'>
                 <div style={{width: '100%',
                  display:'flex',
                  justifyContent:'space-evenly'}}
                 >
-                    <h3 style={{width:'30%', textAlign:'center', fontWeight: 'bold'}}>Peso</h3>
-                    <h3 style={{width:'30%', textAlign:'center', fontWeight: 'bold'}}>Altura</h3>
-                    <h3 style={{width:'30%', textAlign:'center', fontWeight: 'bold'}}>Habilidad</h3>
+                    <h3 className='width30-text-center--bold'>Weight</h3>
+                    <h3 className='width30-text-center--bold'>Height</h3>
+                    <h3 className='width30-text-center--bold'>Ability</h3>
                 </div>
                 <div style={{width: '100%',
                  display:'flex',
                  justifyContent:'space-evenly',
                  alignItems:'center'}}
                 >
-                    <h3 style={{width:'30%', textAlign:'center'}}>122 Kg</h3>
-                    <h3 style={{width:'30%', textAlign:'center'}}>2 m</h3>
-                    <h3 style={{width:'30%', textAlign:'center'}}>Presion</h3>
+                    <h3 className='width30-text-center'>{weight} Kg</h3>
+                    <h3 className='width30-text-center'>{height} m</h3>
+                    <h3 className='width30-text-center'>{ability}</h3>
                 </div>
             </div>
             <div className='tipo-debilidades'>
